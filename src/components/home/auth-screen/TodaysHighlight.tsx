cea1bbbfe19b294efe918047f784106f29c346fa
damian dev <inamida16@gmail.com>
2024-07-15 12:19:17 +0300

    Removed cloudinary video player because of performance

--- src/components/home/auth-screen/TodaysHighlight.tsx
+++ src/components/home/auth-screen/TodaysHighlight.tsx
@@ -4,11 +4,7 @@ import { CldVideoPlayer } from 'next-cloudinary'
 import 'next-cloudinary/dist/cld-video-player.css'
 
 const TodaysHighlight = () => {
-  return (
-    <div className="w-full md:w-3/4 mx-auto">
-      <CldVideoPlayer width="960" height="540" className="rounded-md" src="highlighted-vid_ewywxv" />
-    </div>
-  )
+  return <div className="w-full md:w-3/4 mx-auto"></div>
 }
 
 export default TodaysHighlight
