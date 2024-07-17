d7cddfcc86674d54009e6474e2b8443e4839974d
damian dev <inamida16@gmail.com>
2024-07-17 12:45:32 +0300

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import AuthScreen from '@/components/home/auth-screen/AuthScreen';
-import HomeScreen from '@/components/home/home-screen/HomeScreen';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import AuthScreen from '@/components/home/auth-screen/AuthScreen'
+import HomeScreen from '@/components/home/home-screen/HomeScreen'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export default async function Home() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  return <main>{user ? <HomeScreen /> : <AuthScreen />}</main>;
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  return <main>{user ? <HomeScreen /> : <AuthScreen />}</main>
 }
